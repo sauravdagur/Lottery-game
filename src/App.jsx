8f6e0dffce45675aea3d658ca3da43b21fca577b
sauravdagur <sauravdagur2020@gmail.com>
2026-06-08 19:05:13 +0530

    Fix import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-
-import './App.css'
-import { Ticket } from './ticket'
+import './App.css';
 import Lottery from './Lottery';
 
 function App() {
-  
-
   return (
     <>
-    <p>Sum =15 you will win </p>
-     <Lottery n={3} winningSum={15}/>
+      <p>Sum = 15 you will win</p>
+      <Lottery n={3} winningSum={15} />
     </>
   );
 }
 
-export default App
+export default App;
